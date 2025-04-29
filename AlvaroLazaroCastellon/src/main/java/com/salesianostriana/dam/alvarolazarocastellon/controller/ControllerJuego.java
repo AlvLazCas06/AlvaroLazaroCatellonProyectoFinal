8c78be8753b9dddb3d726a38f89eff8a1ee6387e
Álvaro Lázaro Castellón <alvarolazaro101@gmail.com>
2025-04-30 00:35:08 +0200

    Cambios en los estilos de algunos html, adición de un nuevo atributo en la clase juego, cambios en los métodos de Service Juego y cambios en las propiedades...

--- AlvaroLazaroCastellon/src/main/java/com/salesianostriana/dam/alvarolazarocastellon/controller/ControllerJuego.java
+++ AlvaroLazaroCastellon/src/main/java/com/salesianostriana/dam/alvarolazarocastellon/controller/ControllerJuego.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
@@ -26,7 +27,7 @@ public class ControllerJuego {
 
     @PostMapping("/mostrarjuego")
     public String addGame(Model model, @ModelAttribute("juego") Juego juego) {
-        serviceJuego.addGames(juego);
+        serviceJuego.addGame(juego);
         return "redirect:/mostrarjuego";
     }
 
@@ -34,6 +35,38 @@ public class ControllerJuego {
     public String showGame(Model model) {
         model.addAttribute("juego", serviceJuego.getList());
         return "showgames";
+    }
+
+    @GetMapping("/mostrarjuego/editar/{id}")
+    public String showEditGame(Model model, @PathVariable Long id) {
+        model.addAttribute("juego", serviceJuego.getJuego(id));
+        return "editgame";
+    }
+
+    @PostMapping("/mostrarjuego/{id}")
+    public String editGame(Model model, @PathVariable Long id, @ModelAttribute("juego") Juego j) {
+        Juego juego = serviceJuego.getJuego(id);
+        juego.setId(id);
+        juego.setNombre(j.getNombre());
+        juego.setDescription(j.getDescription());
+        juego.setPrecio(j.getPrecio());
+        juego.setCantidad(j.getCantidad());
+        juego.setVentas(j.getVentas());
+        juego.setNuevo(j.isNuevo());
+        juego.setGenero(j.getGenero());
+        juego.setNumJugadores(j.getNumJugadores());
+        juego.setPlataforma(j.getPlataforma());
+        juego.setFechaLanzamiento(j.getFechaLanzamiento());
+        juego.setLlegadaAlMercado(j.getLlegadaAlMercado());
+        juego.setRutaImagen(j.getRutaImagen());
+        serviceJuego.editGame(juego);
+        return "redirect:/mostrarjuego";
+    }
+
+    @GetMapping("/mostrarjuego/{id}")
+    public String deleteGame(Model model, @PathVariable Long id) {
+        serviceJuego.removeGame(id);
+        return "redirect:/mostrarjuego";
     }
 
     @GetMapping("/catalogo")
