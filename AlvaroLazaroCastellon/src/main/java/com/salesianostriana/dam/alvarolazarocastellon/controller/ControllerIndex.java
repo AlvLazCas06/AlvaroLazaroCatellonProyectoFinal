90d762afbe756ae11f237e06e8e630001d09bcef
Álvaro Lázaro Castellón <alvarolazaro101@gmail.com>
2025-05-14 23:00:37 +0200

    Cambios

--- AlvaroLazaroCastellon/src/main/java/com/salesianostriana/dam/alvarolazarocastellon/controller/ControllerIndex.java
+++ AlvaroLazaroCastellon/src/main/java/com/salesianostriana/dam/alvarolazarocastellon/controller/ControllerIndex.java
@@ -5,7 +5,10 @@ import com.salesianostriana.dam.alvarolazarocastellon.services.ServiceJuego;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 public class ControllerIndex {
@@ -18,6 +21,24 @@ public class ControllerIndex {
         model.addAttribute("masvendido", serviceJuego.findMaxSell().orElse(new Juego()));
         model.addAttribute("threeMaxSell", serviceJuego.findThreeMaxSell());
         return "main";
+    }
+
+    @PostMapping("/carrito/agregar/{id}")
+    public String addGameToCart(@PathVariable Long id, @RequestParam int cantidad, Model model) {
+        Juego juego = serviceJuego.findById(id);
+        juego.setCantidad(cantidad);
+        if (juego != null) {
+            model.addAttribute("juego", juego);
+            return "redirect:/";
+        } else {
+            return "error";
+        }
+    }
+
+    @GetMapping("/carrito")
+    public String showCarrito() {
+        List<Juego> juegos = new ArrayList<Juego>();
+        return "carrito";
     }
 
 }
